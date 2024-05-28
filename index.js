43dd85b0e7e8291a522e46bccd10892d2c72e807
nihistha <nihirastha20@gmail.com>
2024-05-28 22:55:03 +0545

    register backend

--- index.js
+++ index.js
@@ -3,12 +3,23 @@ const express = require('express');
 const dotenv = require("dotenv");
 const mongoose = require('mongoose');
 const connectDB = require('./database/database');
+const cors = require("cors");
+const fileUpload = require('express-fileupload')
 
 //creating an express app
 const app = express();
 
 //Json config
 app.use(express.json());
+
+app.use(fileUpload());
+
+const corsOptions = {
+    origin: true,
+    credentials : true,
+    optionSuccessStatus : 200
+} 
+app.use(cors(corsOptions));
 
 dotenv.config();
 
@@ -17,6 +28,11 @@ connectDB();
 
 //defining a port
 const PORT = process.env.PORT
+
+//creating a test route or endpoint
+app.get('/test',(req,res)=>{
+    res.end("Test Api is working")
+})
 
 //CREATE USER API
 app.use('/api/user',require('./routes/user_routes'))
