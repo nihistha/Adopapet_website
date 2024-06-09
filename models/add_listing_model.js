251222e199d1a5b6674d56c79022b42f192578ed
nihistha <nihirastha20@gmail.com>
2024-06-09 16:23:54 +0545

    add listing model made

---
+++ models/add_listing_model.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose")
+
+const addListingSchema = new mongoose.Schema({
+    petName :{
+        type: String,
+        required: true
+    },
+    petType : {
+        type : String,
+        required: true
+    },
+    breed : {
+        type : String,
+        required: true
+    },
+    gender : {
+        type : String,
+        required: true
+    },
+    size: {
+        type : String,
+        required : true
+    },
+    aboutPet : {
+        type : String,
+        required : true
+    },
+
+})
+
+const pet_listing = mongoose.model("pet",addListingSchema);
+module.exports = pet_listing
